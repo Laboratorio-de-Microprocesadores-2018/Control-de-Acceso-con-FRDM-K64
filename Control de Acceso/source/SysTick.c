54a61bef85abe834cda0209c1242cfb7a3b49879
Tobias <Tobias@10.2.69.37>
2018-08-28 19:53:48 -0300

    Revert "No se usar git"
    This reverts commit 9b4dc4b449904d6d92d15b2532dbf0f276945763.

--- Control de Acceso/source/SysTick.c
+++ Control de Acceso/source/SysTick.c
@@ -1,18 +1,9 @@
 #include "SysTick.h"
 
-typedef struct{
-	SysTickFnc f;
-	uint32_t resetValue;
-	uint32_t count;
-}SysTickCallback;
 
-
-static SysTickCallback callbacks[SYSTICK_MAX_CALLBACKS];
-static int callbacksSize;
-
-uint32_t sysTickInit ()
+uint32_t SysTick_Init (float time)
 {
-	uint32_t ticks = SYSTICK_ISR_PERIOD_S*__CORE_CLOCK__;
+	uint32_t ticks = time*__CORE_CLOCK__;
 
 	if ((ticks - 1UL) > SysTick_LOAD_RELOAD_Msk)
 	    return (0UL);
@@ -22,58 +13,4 @@ uint32_t sysTickInit ()
 	SysTick->VAL  = 0UL;
 	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk|SysTick_CTRL_TICKINT_Msk|SysTick_CTRL_ENABLE_Msk;
 	return (1UL);
-}
-
-bool sysTickAddCallback(SysTickFnc fnc,float period)
-{
-	if(callbacksSize < SYSTICK_MAX_CALLBACKS)
-	{
-		callbacks[callbacksSize].f = fnc;
-		callbacks[callbacksSize].resetValue = period/SYSTICK_ISR_PERIOD_S;
-		callbacks[callbacksSize].count = period/SYSTICK_ISR_PERIOD_S;
-		callbacksSize++;
-		return true;
-	}
-	else return false;
-}
-
-bool sysTickAddDelayCall(SysTickFnc fnc,float time)
-{
-	if(callbacksSize < SYSTICK_MAX_CALLBACKS)
-	{
-		callbacks[callbacksSize].f = fnc;
-		callbacks[callbacksSize].resetValue = 0;
-		callbacks[callbacksSize].count = time/SYSTICK_ISR_PERIOD_S;
-		callbacksSize++;
-		return true;
-	}
-	else return false;
-}
-
-
-__ISR__ SysTick_Handler(void)
-{
-	// Iterate all registered functions
-	for(int i=0;i<callbacksSize;i++)
-	{
-		callbacks[i].count--;
-
-		if(callbacks[i].count==0)
-		{
-			// If timeouts, execute
-			(*callbacks[i].f)();
-			// And either reset the counter, or unregister
-			if(callbacks[i].resetValue!=0)
-				callbacks[i].count = callbacks[i].resetValue;
-			else
-			{
-				for(int j=i+1;j<callbacksSize;j++)
-				{
-					callbacks[j-1]=callbacks[j];
-				}
-				callbacksSize--;
-				i--;
-			}
-		}
-	}
 }
--- Control de Acceso/source/SysTick.h
+++ Control de Acceso/source/SysTick.h
@@ -12,31 +12,34 @@
  ******************************************************************************/
 #include <stdint.h>
 #include "hardware.h"
-#include <stdbool.h>
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
-#define MS2S (1/1000.0)
-// Execution period of systicks callbacks
-#define SYSTICK_ISR_PERIOD_S (10*MS2S)  // 10 ms period
-// Maximum number of callbacks
-#define SYSTICK_MAX_CALLBACKS 10
+
+#define SYSTICK_ISR_FREQUENCY_HZ 100U
+
 
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
-typedef void(*SysTickFnc)(void);
+
+/*******************************************************************************
+ * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 
-// Init SysTick driver
-uint32_t sysTickInit ();
-// Register a periodic call to a function
-bool sysTickAddCallback(SysTickFnc fnc,float period);
-// Register a single call to a function, after a certain delay
-bool sysTickAddDelayCall(SysTickFnc fnc,float time);
-//*******************************************************************************
+/**
+ * @brief Initialize SysTic driver
+ * @param time Time period between calls
+ * @return Function registered OK
+ */
+uint32_t SysTick_Init (float time);
+
+
+/*******************************************************************************
+ ******************************************************************************/
 
 #endif // _SYSTICK_H_
