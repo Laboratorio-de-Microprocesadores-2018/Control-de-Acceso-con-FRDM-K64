1e40ddf68dc0cba8765eb953d56095bff03870bd
tlifschitz <tlifschitz@itba.edu.ar>
2018-09-05 03:45:18 -0300

    Antirebote por soft y gestos de teclado
    Agregue un antirrebote al driver de teclado y la posibilidad de detectar
    si una tecla se apreto muchas veces (MULTICLICK), o un tiempo
    determinado (LONGPRESS)

--- Control de Acceso/source/Keyboard.c
+++ Control de Acceso/source/Keyboard.c
@@ -11,27 +11,33 @@
 //                       Constants and macro definitions                       //
 /////////////////////////////////////////////////////////////////////////////////
 
-#define NCOLS 3U
-#define NROWS 4U
+#define NCOLS 3
+#define NROWS 4
 
 #define COL0 PORTNUM2PIN(PB, 19)
 #define COL1 PORTNUM2PIN(PC, 17)
 #define COL2 PORTNUM2PIN(PB, 18)
 
-#define PRESSED 1
-#define UNPRESSED 0
+#define BUFFER_SIZE 10
 
-#define BUFFER_SIZE 5
+/**
+ * Structure which defines a key in a keyboard
+ */
+typedef struct{
+    int pin;				// Phisical pin where the key is connected.
+    char charCode;			// Character the key represents.
+    bool pressed;			// Stable state (debounced).
+    bool curState;          // Unstable state.
+    bool lastState;         // Previous state.
+    uint8_t clickCount;     // Number of button clicks within multiclickTime.
+    uint64_t lastBounceTime;// The last time the pin was toggled.
+}Key;
 
 /////////////////////////////////////////////////////////////////////////////////
 //                   Local variable definitions ('static')                     //
 /////////////////////////////////////////////////////////////////////////////////
-static int COLS[] = {COL0,COL1,COL2};
-static int kbStatus[NROWS][NCOLS];
-static char kbChars[NROWS][NCOLS] = {{'1','2','3'},
-									 {'4','5','6'},
-									 {'7','8','9'},
-									 {'*','0','#'}};
+
+static Key keys[NROWS][NCOLS];
 
 NEW_CIRCULAR_BUFFER(buffer,BUFFER_SIZE,sizeof(KeyboardEvent));
 
@@ -40,21 +46,37 @@ NEW_CIRCULAR_BUFFER(buffer,BUFFER_SIZE,sizeof(KeyboardEvent));
 /////////////////////////////////////////////////////////////////////////////////
 
 /**
- * Interrupt subroutine called to multiplex the keyboard
+ *	Interrupt subroutine called to multiplex the keyboard.
  */
 static void keyboardPISR(void);
+
+/**
+ *	Function to update key variables and detect states.
+ */
+static void updateKey(Key* key);
+
 
 /////////////////////////////////////////////////////////////////////////////////
 
 void initKeyboard(void)
 {
-	// Set inputs
-	for(int i=0; i<NCOLS; i++)
-		pinMode(COLS[i],INPUT_PULLDOWN);
-	// Set all keys unpressed
-	for(int r=0; r<NROWS; r++)
-		for(int c=0; c<NCOLS; c++)
-			kbStatus[r][c]=UNPRESSED;
+	int COLS[] = {COL0,COL1,COL2};
+
+	char kbChars[NROWS][NCOLS] = {{'1','2','3'},
+								  {'4','5','6'},
+								  {'7','8','9'},
+	 							  {'*','0','#'}};
+	for(int j=0;j<NCOLS; j++)
+	{
+		// Set pins as inputs
+		pinMode(COLS[j],INPUT_PULLDOWN);
+
+		for(int i=0; i<NROWS; i++)
+		{
+			keys[i][j].pin = COLS[j];
+			keys[i][j].charCode = kbChars[i][j];
+		}
+	}
 
 	sysTickAddCallback(&keyboardPISR,(float)(1/(float)MUX_FREQUENCY));
 }
@@ -64,26 +86,10 @@ static void keyboardPISR(void)
 {
 	// Active row
 	int r = getActivePin();
+
 	// Read columns
 	for(int c=0; c<NCOLS; c++)
-	{
-		uint8_t currStatus = digitalRead(COLS[c]);
-		if(currStatus != kbStatus[r][c])
-		{
-			KeyboardEvent ev = {.charCode = kbChars[r][c],
-						.row=r ,
-						.col=c};
-
-			if(currStatus==PRESSED)
-				ev.type=KB_KEY_DOWN;
-			else
-				ev.type=KB_KEY_UP;
-
-			push(&buffer,&ev);
-			kbStatus[r][c] = currStatus;
-		}
-	}
-
+		updateKey(&keys[r][c]);
 }
 
 KeyboardEvent getKeyboardEvent()
@@ -122,3 +128,94 @@ uint8_t keyboardCount()
 	return numel(&buffer);
 }
 
+/*
+		uint8_t currStatus = digitalRead(COLS[c]);
+		if(currStatus != kbStatus[r][c])
+		{
+			KeyboardEvent ev = {.charCode = kbChars[r][c],
+						.row=r ,
+						.col=c};
+
+			if(currStatus==PRESSED)
+				ev.type=KB_KEY_DOWN;
+			else
+				ev.type=KB_KEY_UP;
+
+			push(&buffer,&ev);
+			kbStatus[r][c] = currStatus;
+
+
+
+			KB_KEY_UP,
+				KB_KEY_DOWN,
+				KB_KEY_LONG_PRESS,
+				KB_KEY_SINGLE_PRESS,
+				KB_KEY_MULTI_PRESS,
+				KB_NO_EVT}
+
+
+				*/
+static void updateKey(Key* key)
+{
+	static KeyboardEvent ev;
+
+	// Get current time.
+	uint64_t now = millis();
+
+	// Current button state.
+	key->curState = digitalRead(key->pin);
+
+	// If the keys are active low, invert the logic.
+	if (ACTIVE_LOW) key->curState = ! key->curState;
+
+  	// If the key changed its state, reset the bounce timer.
+	if (key->curState != key->lastState)
+		key->lastBounceTime = now;
+
+	// Debounce the key
+	if (now - key->lastBounceTime > DEBOUNCE_TIME && key->curState != key->pressed)
+	{
+		key->pressed = key->curState;
+		ev.charCode = key->charCode;
+		ev.clicks = 0;
+		if (key->pressed)
+		{
+			ev.type = KB_KEY_DOWN;
+			key->clickCount++;
+		}
+		else
+			ev.type = KB_KEY_UP;
+		push(&buffer,&ev);
+	}
+
+    // If the button released state is stable, report nr of clicks and start new cycle
+	if (!key->pressed && (now - key->lastBounceTime) > MULTI_PRESS_TIME)
+	{
+		ev.charCode = key->charCode;
+		ev.clicks = key->clickCount;
+		switch(key->clickCount)
+		{
+
+		case 1:
+			ev.type = KB_KEY_SINGLE_PRESS;
+			break;
+		case 2:
+			ev.type = KB_KEY_MULTI_PRESS;
+			break;
+		}
+		push(&buffer,&ev);
+		key->clickCount = 0;
+	}
+
+	// Check for "long click"
+	if (key->pressed && (now - key->lastBounceTime > LONG_PRESS_TIME))
+	{
+		ev.charCode = key->charCode;
+		ev.clicks = key->clickCount;
+		ev.type = KB_KEY_LONG_PRESS;
+		push(&buffer,&ev);
+		key->clickCount = 0;
+	}
+
+	key->lastState = key->curState;
+}
--- Control de Acceso/source/Keyboard.h
+++ Control de Acceso/source/Keyboard.h
@@ -20,18 +20,29 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+
+#define DEBOUNCE_TIME 20
+#define MULTI_PRESS_TIME 200
+#define LONG_PRESS_TIME 9000
+#define ACTIVE_LOW 0
+
 /////////////////////////////////////////////////////////////////////////////////
 //                    Enumerations, structures and typedefs                    //
 /////////////////////////////////////////////////////////////////////////////////
 
-typedef enum{KB_KEY_UP,KB_KEY_DOWN,KB_NO_EVT}KeyboardEventType;
+typedef enum{	KB_KEY_UP,
+				KB_KEY_DOWN,
+				KB_KEY_LONG_PRESS,
+				KB_KEY_SINGLE_PRESS,
+				KB_KEY_MULTI_PRESS,
+				KB_NO_EVT} KeyboardEventType;
+
 
 typedef struct
 {
 	KeyboardEventType type;
 	char charCode;
-	int row;
-	int col;
+	uint8_t clicks;
 }KeyboardEvent;
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -47,8 +58,6 @@ KeyboardEvent getKeyboardEvent();
 bool keyboardHit();
 void keyboardFlush();
 uint8_t keyboardCount();
-
-
 
 
 /////////////////////////////////////////////////////////////////////////////////
