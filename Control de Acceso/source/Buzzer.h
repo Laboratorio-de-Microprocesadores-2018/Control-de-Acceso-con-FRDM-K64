1cade4327375c533b22273092fd0f3a95860db90
tlifschitz <tlifschitz@itba.edu.ar>
2018-09-07 02:07:53 -0300

    Buzzer comentado

--- Control de Acceso/source/Buzzer.h
+++ Control de Acceso/source/Buzzer.h
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 /**
  * @file     Buzzer.h
- * @brief    Buzzer driver to make simple bips.
+ * @brief    File containing simple services to make bips with piezzo buzzer.
  */
 
 #ifndef BUZZER_H_
@@ -15,31 +15,45 @@
 //                       Constants and macro definitions                       //
 /////////////////////////////////////////////////////////////////////////////////
 
+/** Time duration of sounds*/
 #define BIP_TIME_MS 200
 #define DOUBLE_BIP_TIME_MS 200
 #define LONG_BIP_TIME_MS 1000
+
+/** Tone for the HIGH_PITCH sound*/
+#define BUZZER_FREQ 2500
+
+/////////////////////////////////////////////////////////////////////////////////
+//                    Enumerations, structures and typedefs                    //
+/////////////////////////////////////////////////////////////////////////////////
+
+/** Desired tone for the sound. LOW is the half frequency of HIGH. */
 typedef enum{HIGH_PITCH,LOW_PITCH}Tone;
+
 /////////////////////////////////////////////////////////////////////////////////
 //                         Global function prototypes                          //
 /////////////////////////////////////////////////////////////////////////////////
 
 /**
- *
+ *	@brief Initializate buzzer.
  */
 void initBuzzer();
 
 /**
- *
+ * @brief Produce a short bip.
+ * @param t Desired tone, LOW_PITCH or HIGH_PITCH
  */
 void bip(Tone t);
 
 /**
- *
+ * @brief Produce a double bip.
+ * @param t Desired tone, LOW_PITCH or HIGH_PITCH
  */
 void doubleBip(Tone t);
 
 /**
- *
+ * @brief Produce a long bip.
+ * @param t Desired tone, LOW_PITCH or HIGH_PITCH
  */
 void longBip(Tone t);
 
